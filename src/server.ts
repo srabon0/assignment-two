d2e1f91a013397665adf079eedf5135999967705
Md Amam Uddin Srabon <47981853+srabon0@users.noreply.github.com>
2024-05-22 12:01:06 +0600

    hide db uri

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,6 @@ async function main() {
   try {
     await mongoose.connect(config.dbUri as string)
     app.listen(config.port, () => {
-      console.log(config.dbUri)
       console.log(`Example app listening on port ${config.port}`)
     })
   } catch (error) {
